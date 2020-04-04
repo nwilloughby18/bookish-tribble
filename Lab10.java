a31d1c14db582609cb946e1dfdc4707ce5d165f5
nwilloughby18 <40634734+nwilloughby18@users.noreply.github.com>
2020-04-04 10:48:28 -0400

    The starter code for the lab
    yeet

---
+++ Lab10.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Lab10
+{
+	public static void main(String[] args) {
+		nesty_MeNumbers();
+		powers();
+		q3();
+	}
+	public static void nesty_MeNumbers() {
+		//code here
+	}
+	public static void powers() {
+		Scanner sr = new Scanner(System.in);
+		System.out.print("Please enter a number: ");
+		int n = sr.nextInt();
+		System.out.println("n"+"\tn^2"+"\tn^3"+"\tn^4");
+		//code here
+	}
+	public static void q3() {
+		System.out.println("Loop1:");
+		//code here
+		
+		
+		
+		System.out.println("Loop2:");
+		//code here
+	}
+}
