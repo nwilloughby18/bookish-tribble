bef3e078faf4158001b3799ffeb1b9e62be33d27
nwilloughby18 <40634734+nwilloughby18@users.noreply.github.com>
2020-04-04 22:46:22 -0400

    Update Lab10.java

--- Lab10.java
+++ Lab10.java
@@ -3,12 +3,12 @@ public class Lab10
 {
     public static void main(String args[]) {
         nesty_MeNumbers();
-		    powers(); 
+	powers(); 
         q3();
     }
      public static void nesty_MeNumbers() {
-		 for (int i=0; i<5; i++)
-		    {
+	for (int i=0; i<5; i++)
+	{
           for(int j=4-i; j>=0; j--){
             System.out.print(" ");
           }
@@ -19,32 +19,32 @@ public class Lab10
         }
     }
 	public static void powers() {
-		Scanner num = new Scanner(System.in);
-		System.out.print("Please enter a number: ");
-		int n = num.nextInt();
-		System.out.println("n"+"\tn^2"+"\tn^3"+"\tn^4");
+	Scanner num = new Scanner(System.in);
+	System.out.print("Please enter a number: ");
+	int n = num.nextInt();
+	System.out.println("n"+"\tn^2"+"\tn^3"+"\tn^4");
 		for(int i=0; i<=n; i++){
 		    System.out.print("");
 		    System.out.println(i + "\t" + i*i + "\t" + i*i*i + "\t" +i*i*i*i);
-	  }
+	 	 }
 	}
 public static void q3() {
-		 System.out.println("Loop1:");
+	System.out.println("Loop1:");
         int val1 = 0;
             for(int i = 1; i<=10; i++){
                 val1 = val1 + i;
                 System.out.println(val1);
             }
-		System.out.println("Loop2:");
-		  int val2 = 0;
-		    for (int i = 1; val2<=100; i++) {
-		    val2 = val2 + i;
-		      if (val2 <100){
-		        System.out.println(val2);
-		      }
-		      if(val2>=100){
+	System.out.println("Loop2:");
+	int val2 = 0;
+	    for (int i = 1; val2<=100; i++) {
+		val2 = val2 + i;
+		   if (val2 <100){
+		      System.out.println(val2);
+		   }
+		   if(val2>=100){
 		        System.out.println("over 100");
-		      }
+		   }
         }
   }
 }
