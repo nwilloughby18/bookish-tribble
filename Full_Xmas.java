9c69e28c97681807c925df95890af47bef8c11a6
nwilloughby18 <40634734+nwilloughby18@users.noreply.github.com>
2020-04-08 23:00:36 -0400

    Create Full_Xmas.java

---
+++ Full_Xmas.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Full_Xmas {
+    public static void main(String args[]) {
+      Scanner tree = new Scanner(System.in);
+      System.out.println("Please enter the desired number of rows: ");
+      int n = tree.nextInt();
+      for(int i = 0; i < n; i++){
+        for(int j = 0; j < n - i; j++)
+            System.out.print(" ");
+        for(int k = 0; k < (1 * i + 1); k++)
+            System.out.print("* ");
+            System.out.println();
+        }
+      }
+     }
